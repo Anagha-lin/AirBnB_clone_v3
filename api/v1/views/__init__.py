205257f6664da7b0e07309807c06bb95fa20929c
Anagha-lin <linusanagha37@.gmailcom>
2024-04-02 20:29:30 +0000

    a

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,16 @@
+#!/usr/bin/python3
+"""Creating a blueprint for the Api."""
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+# Creating a Blueprint instance with url prefix /api/v1
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
-# Import the routes defined in index.py
+# import all views from index module
 from api.v1.views.index import *
-
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,32 @@
-from api.v1.views import app_views
+#!/usr/bin/python3
+"""Defines views for the Airbnb API."""
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 
-@app_views.route("/status", methods=["GET"])
-def status():
+# Define a route /status within the app_views blueprint
+@app_views.route('/status', strict_slashes=False)
+def api_status():
+    """Returns a JSON response indicating the status of the API."""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_statistics():
+    """
+    Retrieves the count of each object type.
+    """
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
+
+
+if __name__ == "__main__":
+    pass
 
