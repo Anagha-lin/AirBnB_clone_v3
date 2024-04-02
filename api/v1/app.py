b6f3d9bb1290885ca3308d32d9bc35789b3b6cf1
Anagha-lin <linusanagha37@.gmailcom>
2024-04-02 20:52:30 +0000

    a

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,33 @@
-from flask import Flask
-from api.v1.views import app_views
+#!/usr/bin/python3
+'''
+This script sets up a Flask application and integrates the blueprint app_views into it.
+'''
+
+from os import environ
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
+from api.v1.views import app_views
 
 app = Flask(__name__)
+
+# Enable CORS and specify allowed origins:
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0:"}})
+
+# Register the blueprint app_views with the Flask app and make URLs non-strict:
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
+    '''
+    Cleans up the current SQLAlchemy Session object after each request.
+    '''
     storage.close()
 
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", 5000)
-    app.run(host=host, port=port, threaded=True)
+
+if __name__ == '__main__':
+    HOST = environ.get('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(environ.get('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
 
